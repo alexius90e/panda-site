53ccc2950e1b59c7691fc10477e7040aee44bc1f
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-10-11 15:40:48 +0300

    feat: implemented contacts

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
 const phoneInputs = document.querySelectorAll('.contact-form__phone');
 
+const maskOptions = {
+  mask: '+{995} 000 000 000',
+};
+
 phoneInputs.forEach((inputElement) => {
-  const maskOptions = {
-    mask: '+{995} 000 000 000',
-  };
   const mask = IMask(inputElement, maskOptions);
 });
+
+const contactsFormInputPhone = document.querySelector('.contacts__form-input_phone');
+
+const contactsFormInputPhoneMask = IMask(contactsFormInputPhone, maskOptions);
 
 const priceItems = document.querySelectorAll('.prices__item');
 
