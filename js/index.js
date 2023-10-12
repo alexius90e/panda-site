d0148a8b85c82776eb0e16f5794485d4208af079
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-10-12 21:54:50 +0300

    feat: implemented gallery

--- js/index.js
+++ js/index.js
@@ -42,7 +42,7 @@ faqItems.forEach((item) =>
 const reviewSlider = new Swiper('.reviews__swiper', {
   loop: true,
   slidesPerView: 1,
-  spaceBetween: 10,
+  spaceBetween: 16,
   setWrapperSize: true,
   breakpoints: {
     640: {
@@ -59,3 +59,32 @@ const reviewSlider = new Swiper('.reviews__swiper', {
     prevEl: '.reviews__slider-btn_prev',
   },
 });
+
+const breakpoint = window.matchMedia('(min-width: 576px)');
+
+let gallerySlider;
+
+const breakpointChecker = function () {
+  if (breakpoint.matches === true) {
+    if (gallerySlider !== undefined) gallerySlider.destroy(true, true);
+    return;
+  } else if (breakpoint.matches === false) {
+    return enableSwiper();
+  }
+};
+
+const enableSwiper = function () {
+  gallerySlider = new Swiper('.gallery__slider', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.gallery__button_next',
+      prevEl: '.gallery__button_prev',
+    },
+  });
+};
+
+breakpoint.addEventListener('change', breakpointChecker);
+
+breakpointChecker();
