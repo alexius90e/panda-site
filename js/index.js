94028085893c3bd032c0bd175b31a48e70ab61cd
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-10-12 23:34:44 +0300

    feat: implemented burger menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+const header = document.querySelector('.header');
+const headerNav = document.querySelector('.header__nav');
+const burgerButton = document.querySelector('.header__burger-btn');
+
+burgerButton.addEventListener('click', () => {
+  header.classList.toggle('active');
+});
+
+headerNav.addEventListener('click', () => {
+  header.classList.remove('active');
+});
+
 const phoneInputs = document.querySelectorAll('.contact-form__phone');
 
 const maskOptions = {
